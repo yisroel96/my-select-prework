71ecedb6bfaf7990bddf7f9b33b67eef46fdb21c
witty-collection-3981 <yisroel96@gmail.com>
2018-11-27 20:35:54 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,11 +1,12 @@
-def my_select(collection)
- i = 0
- select = []
- while i < array.length
-   if yield(array[i])
-     select << array[i]
-   end
-   i+=1
- end
- select
+
+def my_select(array)
+  i = 0
+  select = []
+  while i < array.length
+    if yield(array[i])
+      select << array[i]
+    end
+    i+=1
+  end
+  select
 end
